fa8eb09287389257661201c0c35cb629d156af2c
Zh79 <111058973+Zh79@users.noreply.github.com>
2022-09-05 22:20:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int count = 0;
+int pol(string n)
+{
+    
+    if (n == "STOP") return count;
+    
+    else if (Convert.ToDouble(n) > 0) 
+     count++;
+     System.Console.WriteLine("Введите число. Для подсчета введите (STOP)");
+     return pol(Console.ReadLine());
+     
+}
+System.Console.WriteLine("Введите число. Для подсчета введите (STOP)");
+System.Console.WriteLine(pol(Console.ReadLine()));
+
+System.Console.WriteLine("Для нахождения точки пересечения двух прямых, введите число b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = 0; 
+double y = 0;
+x = (b1-b2) / (k2-k1);
+y = k1*x +b1;
+System.Console.WriteLine($"({x};{y})");
