f3378d8ce6ba845aec31ced74c4e33744583d9a1
Zh79 <111058973+Zh79@users.noreply.github.com>
2022-09-05 22:36:42 +0300

    Update and rename Program.cs to DZ6.cs

--- DZ6.cs
+++ DZ6.cs
@@ -14,7 +14,6 @@ int pol(string n)
 }
 System.Console.WriteLine("Введите число. Для подсчета введите (STOP)");
 System.Console.WriteLine(pol(Console.ReadLine()));
-
 System.Console.WriteLine("Для нахождения точки пересечения двух прямых, введите число b1");
 double b1 = Convert.ToDouble(Console.ReadLine());
 System.Console.WriteLine("Введите число k1");
@@ -25,6 +24,16 @@ System.Console.WriteLine("Введите число k2");
 double k2 = Convert.ToDouble(Console.ReadLine());
 double x = 0; 
 double y = 0;
+if (k1 == k2 & b2 == b1)
+{
+    System.Console.WriteLine("Прямые совпадают");
+}
+else if (k1 == k2)
+{
+    System.Console.WriteLine("Прямые параллельны");
+}
+else {
 x = (b1-b2) / (k2-k1);
 y = k1*x +b1;
 System.Console.WriteLine($"({x};{y})");
+}
