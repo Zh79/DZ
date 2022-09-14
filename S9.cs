c84e4c37760e9e06acd4893b488c71ba179c6260
Zh79 <111058973+Zh79@users.noreply.github.com>
2022-09-14 21:33:16 +0300

    Add files via upload

---
+++ S9.cs
@@ -0,0 +1,32 @@
+﻿//Задача 64: 
+
+string PromNM (int M, int N)
+{
+    string result = string.Empty;
+    if (M>N) return "";
+    else return ($"{M}, {PromNM(M+1,N)}");
+}
+
+System.Console.WriteLine(PromNM(4,10));
+
+//Задача 66: 
+
+int SummNM (int M, int N)
+{
+    if (M>N) return 0;
+    else return M + SummNM(M+1,N);
+}
+
+System.Console.WriteLine(SummNM(4,8));
+
+//Задача 68: 
+
+double Ackermann(double m, double n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return Ackermann(m-1, 1);
+    return Ackermann(m-1, Ackermann(m, n-1));
+}
+
+System.Console.WriteLine(Ackermann(3,3));
+
